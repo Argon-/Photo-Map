45fcb5597d0a51df96d6a56454628e3712dc4e08
Julian <MyFakeAcc.4@googlemail.com>
2014-12-07 17:07:09 +0100

    util: add java.lang.Math version for haversine
    at least with my CPU it's even faster than FastMath

--- src/util/Distance.java
+++ src/util/Distance.java
@@ -16,7 +16,7 @@ public final class Distance
 	static final double EARTH_RADIUS = 6371;
 	
 	
-	public static double haversine(double lat1, double lon1, double lat2, double lon2)
+	public static double haversine_fm(double lat1, double lon1, double lat2, double lon2)
 	{
 		double dLat = FastMath.toRadians(lat2 - lat1);
 		double dLng = FastMath.toRadians(lon2 - lon1);
@@ -27,6 +27,20 @@ public final class Distance
 
 		return EARTH_RADIUS * c;
 	}
+	
+	
+	public static double haversine(double lat1, double lon1, double lat2, double lon2)
+	{
+		double dLat = Math.toRadians(lat2 - lat1);
+		double dLng = Math.toRadians(lon2 - lon1);
+
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+		return EARTH_RADIUS * c;
+	}
+
 
 
 	// don't use
@@ -41,6 +55,60 @@ public final class Distance
 
 	
 	public static void main(String[] args) throws InvalidGraphFormatException, IOException
+	{
+		System.out.println("Distance Test");
+		Graph g = GraphFactory.load("./15000K.bin");
+		
+		double lat_ref = 48.74670985863194;
+		double lon_ref = 9.105284214019775;
+		
+		double[] lats = g.getLatArray();
+		double[] lons = g.getLonArray();
+		
+		double r2_c_max = Double.MIN_VALUE;
+		double r2_d_max = Double.MIN_VALUE;
+
+
+		long t1 = 0, acc1 = 0, t2 = 0, acc2 = 0;
+		double avg1 = 0.0, avg2 = 0.0;
+		final int num_iterations = 100;
+		
+		System.out.println("\nHaversine FastMath vs Math");
+		System.out.println(num_iterations + " Iterations, results in seconds\n");
+		
+		for (int iteration = 0; iteration < num_iterations; ++iteration)
+		{
+			for (int i = 0; i < lats.length; ++i)
+			{
+				t1 = System.nanoTime();
+				double r1 = haversine_fm(lat_ref, lon_ref, lats[i], lons[i]);
+				acc1 += System.nanoTime() - t1;
+				
+				t2 = System.nanoTime();
+				double r2 = haversine(lat_ref, lon_ref, lats[i], lons[i]);
+				acc2 += System.nanoTime() - t2;
+				
+				double r2_change = Math.abs(r2 - r1) / r1;
+				double r2_diff   = Math.abs(r2 - r1);
+				
+				r2_c_max = FastMath.max(r2_c_max, r2_change);
+				r2_d_max = FastMath.max(r2_d_max, r2_diff);
+			}
+			
+			System.out.println(String.format("org.apache.commons.math3.util.FastMath: %.6f \t java.lang.Math: %.6f", acc1 / 1000000000.0, acc2 / 1000000000.0));
+			avg1 += acc1;
+			avg2 += acc2;
+			acc1 = 0;
+			acc2 = 0;
+		}
+		avg1 /= num_iterations;
+		avg2 /= num_iterations;
+		System.out.println("\nAverage:");
+		System.out.println(String.format("org.apache.commons.math3.util.FastMath: %.6f \t java.lang.Math: %.6f", avg1 / 1000000000.0, avg2 / 1000000000.0));
+	}
+	
+	
+	public static void main2(String[] args) throws InvalidGraphFormatException, IOException
 	{
 		System.out.println("Distance Test");
 		Graph g = GraphFactory.load("./15000K.bin");
@@ -71,4 +139,5 @@ public final class Distance
 			System.out.println(String.format("%.3f \t %.3f (%.3f)  max change = %.3f  max = %.3f", r1, r2_change, r2, r2_c_max, r2_d_max));
 		}
 	}
+
 }
