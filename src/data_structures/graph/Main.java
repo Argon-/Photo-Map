ff3da9a7d169c4929d4971072f48525112670b86
Julian <MyFakeAcc.4@googlemail.com>
2014-12-11 00:57:36 +0100

    project: additional main for reasons I've long forgotten

---
+++ src/data_structures/graph/Main.java
@@ -0,0 +1,14 @@
+package data_structures.graph;
+
+import java.io.IOException;
+
+
+public class Main
+{
+
+	public static void main(String[] args) throws InvalidGraphFormatException, IOException
+	{
+		//ArrayRepresentation g = GraphFactory.loadArrayRepresentation("./15K.txt");
+	}
+
+}
