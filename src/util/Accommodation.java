08577710d323098cbda53ad12de4c18ac9a89328
Julian <MyFakeAcc.4@googlemail.com>
2015-04-24 01:31:51 +0200

    Accommodation: new convenience class
    To be used later in MainWindow.
    It's basically just a collection of some values.

---
+++ src/util/Accommodation.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.awt.Color;
+
+import gui.overlay.OverlayAggregate;
+import gui.overlay.OverlayElement;
+import gui.overlay.OverlayLabel;
+
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+
+
+/**
+ * Convenience class to encapsulate relevant data for a accommodation.
+ */
+public class Accommodation
+{
+    public final static int STROKE_WIDTH = 2 * 6;
+    
+    private final GeoPosition gp;
+    private final String name;
+    private final OverlayAggregate oa;
+    
+    
+    public Accommodation(GeoPosition gp, String name)
+    {
+        this.gp = gp;
+        this.name = name;
+        oa = new OverlayAggregate();
+        oa.addPoint(new OverlayElement(gp, Color.BLUE, STROKE_WIDTH));
+        oa.addLabel(new OverlayLabel(name, gp, STROKE_WIDTH - 2));
+    }
+    
+    
+    public GeoPosition getPos()
+    {
+        return gp;
+    }
+    
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    
+    public OverlayAggregate getOverlay()
+    {
+        return oa;
+    }
+
+}
