ab25f1b9ea0a63510a3e2106bd16130daa2204c9
Julian <MyFakeAcc.4@googlemail.com>
2015-04-26 15:52:30 +0200

    guimain: properly format code

--- src/gui/GUIMain.java
+++ src/gui/GUIMain.java
@@ -18,12 +18,29 @@ public class GUIMain
 
 					System.out.println("Mirroring stdout to GUI");
 			        System.setOut(new PrintStream(original) {
-			        	public void println()         { original.print(System.getProperty("line.separator")); if (w != null) w.log(System.getProperty("line.separator")); }
-			            public void println(String s) { original.print(s + System.getProperty("line.separator")); if (w != null) w.log(s + System.getProperty("line.separator")); }
-			            public void print(String s)   { original.print(s); if (w != null) w.log(s); }
+                        public void println()
+                        {
+                            original.print(System.getProperty("line.separator"));
+                            if (w != null)
+                                w.log(System.getProperty("line.separator"));
+                        }
+
+                        public void println(String s)
+                        {
+                            original.print(s + System.getProperty("line.separator"));
+                            if (w != null) {
+                                w.log(s + System.getProperty("line.separator"));
+                            }
+                        }
+
+                        public void print(String s)
+                        {
+                            original.print(s);
+                            if (w != null)
+                                w.log(s);
+                        }
 			        });
 			        
-			        //w.init();
 					w.setVisible(true);
 				}
 				catch (Exception e) {
